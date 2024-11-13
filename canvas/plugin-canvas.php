f41bd7e38f18782ace68e87428405d481b01ed85
Moutquin <e2155871@cmaisonneuve.qc.ca>
2024-11-13 09:36:27 -0500

    ajout plugin canvas

---
+++ canvas/plugin-canvas.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Canvas
+ * Plugin URI: https://gftnth00.mywhc.ca/tim14/
+ * Description: Ajout d'un canvas dans une page web
+ * Version: 1.0
+ * Author: Lohan Moutquin
+ * Author URI: https://www.behance.net/lohanmoutquin
+ * License: GPL2
+ */
+
+ function enqueue_style_script(){
+    wp_enqueue_style('plugin-Canvas.css', plugins_url('style.css', __FILE__));
+    wp_enqueue_script('animPerso', plugins_url('js/plugin-AnimPerso.js', __FILE__), array('jquery'), null, true);
+    wp_enqueue_script('codeAccueil', plugins_url('js/plugin-codeAccueil.js', __FILE__), array('jquery'), null, true);
+    wp_enqueue_script('codeCanvas', plugins_url('js/plugin-codeCanvas.js', __FILE__), array('jquery'), null, true);
+}
+
+add_action('wp_enqueue_scripts','enqueue_style_script');
+
+function shortcodeCanvas($atts) {
+    return '<img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/EricG.png" alt="inshallah" id="perso" />
+    <canvas width="900" height="900"></canvas>';
+}
+
+add_shortcode('le_canvas', 'shortcodeCanvas');
