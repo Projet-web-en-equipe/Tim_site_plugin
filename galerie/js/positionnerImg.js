b32372e1113608527934bd5d9d2844885b8301f4
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-11-18 22:49:02 -0500

    ajout animation avec espace

--- galerie/js/positionnerImg.js
+++ galerie/js/positionnerImg.js
@@ -1,7 +1,9 @@
 var groupeImg = document.getElementsByClassName("imgGalerie");
+var ctnGalerie = document.getElementById("ctnGalerie");
 
 for(i=0; i < groupeImg.length; i++){
     groupeImg[i].style.left = "50%";
     groupeImg[i].style.marginLeft = groupeImg[i].clientWidth / -2 + "px"
-    groupeImg[i].style.zIndex = i + "";
+    groupeImg[i].style.zIndex = (groupeImg.length - 1 - i)*1000 + "";
+    // groupeImg[i].style.transformOrigin = ctnGalerie.getBoundingClientRect().x 
 }
--- galerie/js/switchImg.js
+++ galerie/js/switchImg.js
@@ -2,24 +2,40 @@ var flecheG = document.getElementById("flecheG");
 var flecheD = document.getElementById("flecheD");
 var ordre = [];
 
+console.log(groupeImg)
+
 for(i=0; i < groupeImg.length; i++){
-    var bhay = i;
+    var bhay = (groupeImg.length - 1 - i);
     ordre.push(bhay);
 }
+console.log(ordre)
 
 flecheG.addEventListener("click", () => {
     var bhay = ordre[0];
     ordre.shift();
     ordre.push(bhay);
-    for(i=0; i < groupeImg.length; i++){
-        groupeImg[i].style.zIndex = ordre[i] + "";
-    }
+    console.log(ordre)
+    setZImg();
 });
 
 flecheD.addEventListener("click", () => {
-    var bhay = ordre.pop();;
-    ordre.unshift(bhay)
-    for(i=0; i < groupeImg.length; i++){
-        groupeImg[i].style.zIndex = ordre[i] + "";
-    }
+    var bhay = ordre.pop();
+    ordre.unshift(bhay);
+    console.log(ordre)
+    setZImg();
 });
+
+document.addEventListener("keydown", (e)=>{
+    if(e.code == "Space"){
+        groupeImg[ordre.indexOf(2)].classList.add("skip");
+        window.setTimeout(() => {
+            groupeImg[ordre.indexOf(2)].classList.remove("skip");
+        }, 1500)
+    }
+})
+
+function setZImg(){
+    for(var i = 0; i < groupeImg.length; i++){
+        groupeImg[i].style.zIndex = ordre[i];
+    }
+}
