c6670663d22e0be6f24b5f9e4fefcbecc819e296
LohanMout <e2155871@cmaisonneuve.qc.ca>
2024-12-08 19:32:21 -0500

    correction fonction dans le site + commentaires

--- galerie/js/positionnerImg.js
+++ galerie/js/positionnerImg.js
@@ -1,21 +1,35 @@
-var groupeImg = [];
+var groupeImg = []; 
 var ctnGalerie;
 var lesImg;
 
-window.addEventListener("load", appliquerImg);
+//window.addEventListener("load", appliquerImg);
 
 function appliquerImg(){
+    //on va chercher toutes les images dans la categorie
     lesImg = document.getElementsByClassName("imgGalerie");
+    //on va chercher le conteneur de la galerie
     ctnGalerie = document.getElementById("ctnGalerie");
+    //reset le array des image a chaque fois quon remplace les images
     groupeImg = [];
+    //pour toutes les images qui ont ete chercher:
     for(var i = 0; i < lesImg.length; i++){
+        if(lesImg[i].firstChild != null){
+        //on ditch le parent de l'image pour uniquement avoir la balise IMG dans le array
+        console.log(lesImg[0].firstChild);
         groupeImg.push(lesImg[i].firstChild);
+        //on leur donne leur zindex initial
         lesImg[i].firstChild.style.zIndex = (lesImg.length - 1 - i)*1000 + 1000 + "";
+        //on met l'image dans le conteneur des images
         ctnGalerie.appendChild(lesImg[i].firstChild);
+        }
+        else{
+            console.log("sa pue...");
+        }
     }
     ordonnerOrdre();
 }
 
+//simple fonction pour arranger l'ordre des images a chaque reset
 function ordonnerOrdre(){
     ordre = [];
     for (i = 0; i < groupeImg.length; i++) {
--- galerie/js/switchImg.js
+++ galerie/js/switchImg.js
@@ -3,6 +3,7 @@ var flecheD = document.getElementById("flecheD");
 var ordre = [];
 var enAnim = false;
 
+//arranger le zindex des images dans la galerie selon l'ordre
 function setZImg() {
     for (var i = 0; i < groupeImg.length; i++) {
         groupeImg[i].style.zIndex = ordre[i] * 1000 + 1000;
@@ -11,14 +12,22 @@ function setZImg() {
 
 document.body.addEventListener('click', function (event) {
     setZImg();
+    //chercher toute les classes de ce qui a ete toucher
     event.target.classList.forEach(element => {
+        //si cest la fleche droite:
         if (element == "D") {
+            //si l'animation des images qui change n'est pas en cours
             if (!enAnim) {
                 enAnim = true;
+                //ajouter a la premiere image la class qui a l'animation css 
                 groupeImg[ordre.indexOf(groupeImg.length - 1)].classList.add("skip");
+                //lorsque l'anim est fini:
                 window.setTimeout(() => {
+                    //enlever a la premiere image la class qui a l'animation css 
                     groupeImg[ordre.indexOf(groupeImg.length - 1)].classList.remove("skip");
+                    //enlever le dernier chiffre de l'ordre et l'enregistrer
                     var bhay = ordre.pop();
+                    //remettre le chiffre enregistrer au debut de l'ordre
                     ordre.unshift(bhay);
                     setZImg();
                     enAnim = false;
@@ -27,14 +36,18 @@ document.body.addEventListener('click', function (event) {
                 console.log("nuh uh");
             }
         }
+        //si cest la fleche gauche:
         if (element == "G") {
+            //si l'animation des images qui change n'est pas en cours
             if (!enAnim) {
                 enAnim = true;
+                //ajouter a la premiere image la class qui a l'animation css 
                 groupeImg[ordre.indexOf(0)].classList.add("avant");
                 window.setTimeout(() => {
                     groupeImg[ordre.indexOf(0)].classList.remove("avant");
-                    var bhay = ordre[0];
-                    ordre.shift();
+                    //elever le premier chiffre et l'enregistrer
+                    var bhay = ordre.shift();
+                    //remettre le chiffre enregistrer a la fin
                     ordre.push(bhay);
                     setZImg();
                     enAnim = false;
